6676523dfe018669c1426b1493785673428e1083
Sergo-Shmel <150228006+Sergo-Shmel@users.noreply.github.com>
2025-05-05 15:53:43 +0300

    Update TelegramArticleBot.java

--- src/main/java/TelegramArticleBot.java
+++ src/main/java/TelegramArticleBot.java
@@ -315,7 +315,7 @@ public class TelegramArticleBot extends TelegramLongPollingBot {
         gen.setCallbackData("ACT_GEN");
         InlineKeyboardButton rew = new InlineKeyboardButton("✍️ Переписать");
         rew.setCallbackData("ACT_REWRITE");
-        String where = (ch == ChannelType.TG) ? "Telegram" : "Дзен";
+        String where = (ch == ChannelType.TG) ? "Telegram" : "Сайтом";
         InlineKeyboardMarkup kb = new InlineKeyboardMarkup(
                 Arrays.asList(
                         Collections.singletonList(gen),
